b3858d9e843b9901ba3372bdb4963089fef40ff9
Caleb Owens <caleb@gitbutler.com>
2025-05-08 13:02:22 +0200

    Initial experimentation

---
+++ packages/svelte/tests/runtime-runes/samples/effect-inside-derived-only-cleanup/main.svelte
@@ -0,0 +1,18 @@
+<script>
+	let count = $state(0);
+
+	const x = $derived.by(() => {
+		count;
+
+		$effect(() => {
+			console.log('subscribed');
+			return () => {
+				console.log('unsubscribed');
+			};
+		});
+	});
+
+	$effect(() => x);
+</script>
+
+<button onclick={() => (count += 1)}>clickme</button>
---
+++ packages/svelte/tests/runtime-runes/samples/effect-inside-two-derived-only-cleanup/main.svelte
@@ -0,0 +1,22 @@
+<script>
+	let count = $state(0);
+
+	const x = $derived.by(() => {
+		count;
+
+		const y = $derived.by(() => {
+			$effect(() => {
+				console.log("subscribed")
+				return () => {
+					console.log("unsubscribed")
+				}
+			});
+		});
+
+		y;
+	});
+
+	$effect(() => x);
+</script>
+
+<button onclick={() => (count += 1)}>clickme</button>
